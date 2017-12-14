5a6b85c46aa88e7bfc0f7c63f1c33cf116d8704a
Sander Koenders <sanderkoenders@gmail.com>
2017-12-14 16:02:18 +0100

    Feat(WebpackConfig): Add class to generate webpack configuration

---
+++ src/webpackConfig.ts
@@ -0,0 +1,68 @@
+import * as path from 'path';
+import { ContextReplacementPlugin, Configuration } from 'webpack';
+
+export default (projectRoot: string): Configuration => {
+  const config = {
+    entry: {
+      polyfills: path.join(projectRoot, "src", "polyfills.ts"),
+      vendor: path.join(projectRoot, "stryker", "vendor.ts"),
+      app: path.join(projectRoot, "src", "polyfills"),
+      test: path.join(projectRoot, "stryker", "karma-test-shim.js")
+    },
+
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+
+    output: {
+      path: path.join(projectRoot, "out"),
+      filename: '[name].bundle.js',
+      chunkFilename: '[id].chunk.js'
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loaders: [
+            {
+              loader: 'awesome-typescript-loader',
+              options: {
+                configFileName: path.join(projectRoot, 'tsconfig.json')
+              }
+            }, 'angular2-template-loader'
+          ]
+        },
+        {
+          test: /\.html$/,
+          loader: 'html-loader'
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+          loader: 'null-loader'
+        },
+        {
+          test: /\.css$/,
+          exclude: path.join(projectRoot, 'src', 'app'),
+          loader: 'null-loader'
+        },
+        {
+          test: /\.css$/,
+          include: path.join(projectRoot, 'src', 'app'),
+          loader: 'raw-loader'
+        }
+      ]
+    },
+
+    plugins: [
+      new ContextReplacementPlugin(
+        // The (\\|\/) piece accounts for path separators in *nix and Windows
+        /angular(\\|\/)core(\\|\/)@angular/,
+        path.join(projectRoot, 'src'), // location of your src
+        {} // a map of your routes
+      )
+    ]
+  };
+
+  return config as Configuration;
+}
